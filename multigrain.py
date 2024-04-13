b990750022582441d565db511edde9286dfa4281
Gary Bishop <gary.bishop.unc@gmail.com>
2024-04-13 08:34:35 -0400

    98% whole grain. Great loaf.

--- multigrain.py
+++ multigrain.py
@@ -21,10 +21,9 @@
 │     ww/leaven │  10.0 │  50.0 │ ww/leaven     │        │         │
 │           rye │   5.0 │  25.0 │ rye           │  775.0 │  1500.1 │
 │ potato flakes │   3.0 │  15.0 │ potato flakes │  790.0 │  1515.1 │
-│ flaxseed meal │   2.0 │  10.0 │ flaxseed meal │  800.0 │  1525.1 │
-│           vwg │   2.0 │  10.0 │ vwg           │  810.0 │  1535.1 │
-│  red rye malt │   1.0 │   5.0 │ red rye malt  │  815.0 │  1540.1 │
-│   bread flour │  17.0 │  85.0 │ bread flour   │  900.0 │  1625.1 │
+│ flaxseed meal │   3.0 │  15.0 │ flaxseed meal │  805.0 │  1530.1 │
+│           vwg │   2.0 │  10.0 │ vwg           │  815.0 │  1540.1 │
+│            ww │  17.0 │  85.0 │ ww            │  900.0 │  1625.1 │
 │               │       │       │               │        │         │
 │ ─────Add───── │       │       │               │        │         │
 │               │       │       │               │        │         │
@@ -41,30 +40,26 @@
 
 # My usual loaf
 
-This is a single 9 inch loaf. I add 1% red rye malt and only walnuts.
-
-I make the leaven from a newly created batch of NMNF starter. I used 4g of the
-starter with 50g of water and 50g of WW flour. I mixed it about 21:30 and by 8:30 it
-had doubled and it's pH was 4.08.
+I made the leaven from my NMNF in 3 steps starting with 4g at 84F then
+refrigerated it overnight. Resulting pH=4.05. Is it worth the trouble?
 
 I'm using the / in the table to indicate an ingredient that is already
 accounted for. In the variable names I use __ (2 underscores) to indicate this.
 
 I rearranged the order so I could be sure everything is well mixed.
 
-This loaf features walnuts. Really tastes great.
+This loaf features walnuts and pecans.
 
 93F water mixed with the 68F starter and flours produced DT = 78F. Mixed,
 rested 30 minutes, kneaded 8, added roughly broken nuts, kneaded 3 more. S&F in
 the bowl every 45 minutes for 3 cycles.
 
-Shaped and into pan. Proof took about 45 minutes. Baked at 350F starting cold
-with lid on for 35 off for 15.
+Shaped and into pan. Proof took about 60 minutes. Baked at 350F starting cold
+with lid on for 30 off for 20.
 
-Really good loaf. Delicious. Very soft. I don't taste the red rye malt on the
-first day.
+Really good loaf. Delicious. Very soft. Good flavor and texture.
 
-02 April 2024
+12 April 2024
 """
 
 from recipe import R, TBD, water, flour
@@ -89,10 +84,9 @@ R += 100 == R.sum(
     ww__leaven=flour(R.leaven, 100),
     rye=5,
     potato_flakes=3,
-    flaxseed_meal=2,
+    flaxseed_meal=3,
     vwg=2,
-    red_rye_malt=1,
-    bread_flour=TBD,
+    ww=TBD,
 )
 
 R += "Add"
