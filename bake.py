567241c11488386fef44812dd85d8817ba4ccbb6
Gary Bishop <gary.bishop.unc@gmail.com>
2024-05-11 21:03:39 -0400

    Figuring out textx

--- bake.py
+++ bake.py
@@ -1,5 +1,42 @@
 from textx import metamodel_from_file
 
-bake_meta = metamodel_from_file("bake.tx")
 
-bake_model = bake_meta.model_from_file("try.bake")
+class bake:
+    def __init__(self):
+        self.meta = metamodel_from_file("bake.tx")
+        self.meta.register_obj_processors(
+            {
+                "Value": self.handleValue,
+                "NamedValue": self.handleNamedValue,
+                "PartName": self.handlePartName,
+            }
+        )
+
+    def compile(self, name):
+        print("compiling", name)
+        self.model = self.meta.model_from_file(name)
+
+    def handleProduct(self, v):
+        value = v.value
+
+    def handleValue(self, v):
+        if v.named:
+            v.value = v.named.value
+        print("value", v.value)
+
+    def handleNamedValue(self, v):
+        if v.ingredient:
+            v.value = v.part + "." + v.ingredient
+        else:
+            v.value = self.partName + "." + v.part
+
+    def handlePartName(self, v):
+        self.partName = v.name
+        print("partName", v.name)
+
+    def handlePart(self, v):
+        print("part")
+
+
+Baker = bake()
+Baker.compile("try.bake")
