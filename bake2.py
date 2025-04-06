af8f3fd8bc560f65eab09182c2e3f26ebe6ca8b3
Gary Bishop <gary.bishop.unc@gmail.com>
2025-04-06 15:27:02 -0400

    WIP

--- bake2.py
+++ bake2.py
@@ -4,8 +4,8 @@ Bake.py - bread recipes using relationships rather than spreadsheets.
 Gary Bishop July-December 2024
 """
 
-from ingredients import getIngredient
 from lark import Lark, Visitor, Tree, Token, visitors
+from formula import Formula
 import lark
 import numpy as np
 from numpy.typing import NDArray
@@ -13,6 +13,8 @@ from typing import Tuple, Dict, List
 import argparse
 import re
 import sys
+
+formula = Formula()
 
 
 def P(*args):
@@ -23,7 +25,7 @@ def P(*args):
 grammar = r"""
 start: part+
 
-part: ID [ "^" margin ] ":" (hydration | relation | ingredient)+
+part: ID [ "^" margin ] ":" (hydration | relation | unknown)+
 
 margin: NUMBER -> fixed_loss
       | NUMBER "%" -> scaled_loss
@@ -33,22 +35,22 @@ hydration: "hydration" "=" NUMBER
 relation: sum "=" sum
 
 ?sum: product "+" sum   -> add
-   | product "-" sum   -> subtract
-   | product
+    | product "-" sum   -> subtract
+    | product
 
 bp: NUMBER "%"
 
 ?product: bp
-       | term
-       | term "*" product -> multiply
-       | term "/" product -> divide
+        | term
+        | term "*" product -> multiply
+        | term "/" product -> divide
 
-?term: ingredient
-    | NUMBER -> constant
-    | NUMBER "%" -> percent
-    | "(" sum ")"
+?term: unknown
+     | NUMBER -> constant
+     | NUMBER "%" -> percent
+     | "(" sum ")"
 
-ingredient: ID
+unknown: ID
        | ID "." ID
 
 ID: /[a-zA-Z_][a-zA-Z_0-9]*/
@@ -94,28 +96,15 @@ except AssertionError as e:
     raise (e)
 
 
-class CollectPartNames(visitors.Visitor):
-    def part(self, args):
-        ST.addPart(args[0])
-
-
-class AddPartName(visitors.Transformer):
-    def __init__(self, partname):
-        super()
-        self.partname = partname
-
-    def ingredient(self, args):
-        name = args[0]
-        if name.startswith("."):
-            name = self.partname + name
-        return Tree("ingredient", [name])
+def TF():
+    return Tree("unknown", [("dough", "total_flour")])
 
 
 class TransformNumbers(visitors.Transformer):
     def bp(self, args):
         return Tree(
             "multiply",
-            [args[0] / 100.0, "dough.total_flour"],
+            [args[0] / 100.0, TF()],
         )
 
     def NUMBER(self, value):
@@ -127,18 +116,29 @@ class TransformNumbers(visitors.Transformer):
     def ID(self, value):
         return str(value)
 
-    def ingredient(self, args):
+    def unknown(self, args):
         if len(args) == 1:
-            return Tree("ingredient", ["." + args[0]])
+            return Tree("unknown", [args[0]])
         else:
-            return Tree("ingredient", [".".join(args)])
+            return Tree("unknown", [tuple(args)])
 
     def part(self, args):
-        P("part", args)
-        if args[1] is None:
-            args[1] = 0
-        t = Tree("part", args)
-        return AddPartName(args[0]).transform(t)
+        partname, loss, *rest = args
+        if loss is None:
+            loss = 0
+
+        class T(visitors.Transformer):
+            def unknown(self, args):
+                if len(args) == 1:
+                    unknown = formula.addUnknown(partname, args[0])
+                else:
+                    unknown = formula.addUnknown(*args)
+                return Tree("unknown", unknown)
+
+        t = T()
+        rest = [t.transform(arg) for arg in rest]
+        part = formula.addPart(partname, loss)
+        return Tree("part", [partname, part, *rest])
 
 
 P(TransformNumbers().transform(tree).pretty())
